dd40fae934a0ed0f40397603c99af041fdea8d91
Jk Lorania <jhessakey@gmail.com>
2022-04-30 01:40:07 +0800

    Updates

---
+++ precache-manifest.e3e2e79d7f01ceab52505344f39bd6b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1c77be628ae96b770ac",
+    "url": "/static/css/main.540d17b4.chunk.css"
+  },
+  {
+    "revision": "b1c77be628ae96b770ac",
+    "url": "/static/js/main.85c126ac.chunk.js"
+  },
+  {
+    "revision": "85b68fe97a559349cb7a",
+    "url": "/static/js/runtime~main.0c677e9e.js"
+  },
+  {
+    "revision": "05b371f05624aa5f8780",
+    "url": "/static/css/2.e90faa7c.chunk.css"
+  },
+  {
+    "revision": "05b371f05624aa5f8780",
+    "url": "/static/js/2.c0ea407f.chunk.js"
+  },
+  {
+    "revision": "00330752ade665012574",
+    "url": "/static/js/3.9881a070.chunk.js"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "73af04ab63ddcd877f5d18d8e6cc5a1b",
+    "url": "/index.html"
+  }
+];
